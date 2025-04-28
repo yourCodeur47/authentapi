af1ceb60ce8d1fa75d357437ffa7602cb583d6fe
codeur47 <yoropapers@gmail.com>
2025-04-28 23:23:04 +0200

    add sagger configuration

---
+++ src/main/java/io/yorosoft/authentapi/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package io.yorosoft.authentapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
